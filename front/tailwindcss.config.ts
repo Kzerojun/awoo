4a4b0fffd5c0c59b6afa4e0308cd799a73d76e68
daeun <danivalen@naver.com>
2025-03-17 13:57:01 +0900

    🚧 [FE] cont: tailwind 적용 폴더 목록록 추가

--- front/tailwindcss.config.ts
+++ front/tailwindcss.config.ts
@@ -2,6 +2,9 @@ const tailwindConfig = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}", // ✅ Next.js App Router 적용
     "./src/**/*.{js,ts,jsx,tsx}", // ✅ 전체 프로젝트 감지
+    "./src/domains/**/*.{js,ts,jsx,tsx}", // ✅ 비즈니스 로직 관련 컴포넌트 포함
+    "./src/styles/**/*.{css,scss}", // ✅ Tailwind 관련 CSS 파일 감지
+    "./src/common/**/*.{js,ts,jsx,tsx}", // ✅ 공통 UI, Hooks 포함
   ],
   theme: {
     extend: {
