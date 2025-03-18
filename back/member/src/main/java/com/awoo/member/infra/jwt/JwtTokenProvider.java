53a73c08bde6e5be162bb9252500a694c62a3a85
deokjin <ghaehfdl7@naver.com>
2025-03-18 09:40:04 +0900

    🔧 [BE] conf: 설정 파일 수정 ################ # 형식 예시 # ✨ [BE] feat: description # 규칙 : 제목은 50자 이내 / 변경사항이 무엇인지 명확히 작성 / 끝에 마침표 금지 # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 템플릿
    # ✨ [BE] feat: 새로운 기능 추가
    # 🚧 [BE] cont: 기능의 변경으로 인해 코드 변경
    # ♻️ [BE] refactor: 리팩토링 (기능은 그대로인데 코드만 바뀐 경우)
    # 🔧 [BE] conf: CONFIG 파일 수정
    # ➕ [BE] add: 파일 추가
    # ➖ [BE] remove: 파일 제거
    # ✏️ [BE] typos: 오타 수정
    # 📝 [BE] docs: 문서작성 및 편집
    # 🏗️ [BE] structure: 프로젝트 구성 요소 변경
    # ⚡️ [BE] performance: 성능 개선
    # 🐛 [BE] fix: 버그 수정
    # 🚑 [BE] hotfix: 치명적인 버그 수정
    # 🎉 [BE] init: 프로젝트 개시
    # 🚀 [BE] deploy: 배포 변경 사항 및 배포
    # 🔀 [BE] merge: merge conflict 발생 시 merge 전용
    ################

---
+++ back/member/src/main/java/com/awoo/member/infra/jwt/JwtTokenProvider.java
@@ -0,0 +1,43 @@
+package com.awoo.member.infra.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    @Value("${JWT_SECRET_KEY}")
+    private String secretKey;
+    private final long validityInMilliseconds = 3600000; // 1시간
+    private final JwtProperties jwtProperties;
+
+    private Key getSignKey(String secretKey) {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createToken(String memberId, String email) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(memberId)               // 보통 subject에 userId나 식별자 저장
+                .claim("email", email)             // 커스텀 클레임
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(getSignKey(secretKey), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // 만약 토큰 검증, 토큰에서 값 추출 로직이 필요하면 추가 작성
+}
+
