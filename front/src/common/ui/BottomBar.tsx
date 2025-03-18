89729f57bc32fb4e5d445540ab243abdd5ade570
daeun <danivalen@naver.com>
2025-03-18 12:06:34 +0900

    ✨ [FE] feat: 하단바 기능 구현

--- front/src/common/ui/BottomBar.tsx
+++ front/src/common/ui/BottomBar.tsx
@@ -1,33 +1,32 @@
 "use client";
 
 import { usePathname, useRouter } from "next/navigation";
-import Image from "next/image";
 import Link from "next/link";
 
 const navItems = [
   {
     name: "홈",
     path: "/home",
-    activeIcon: "/icons/bottombar/active/home_aqua.png",
-    inactiveIcon: "/icons/bottombar/deactive/home_gray.png",
+    activeIcon: "/icons/bottombar/active/home_aqua.svg",
+    inactiveIcon: "/icons/bottombar/deactive/home.svg",
   },
   {
     name: "중고거래",
     path: "/market",
-    activeIcon: "/icons/bottombar/active/Handshake_aqua.png",
-    inactiveIcon: "/icons/bottombar/deactive/handshake_gray.png",
+    activeIcon: "/icons/bottombar/active/market_aqua.svg",
+    inactiveIcon: "/icons/bottombar/deactive/market.svg",
   },
   {
     name: "산책",
     path: "/walk",
-    activeIcon: "/icons/bottombar/active/walk_aqua.png",
-    inactiveIcon: "/icons/bottombar/deactive/walk_gray.png",
+    activeIcon: "/icons/bottombar/active/walk_aqua.svg",
+    inactiveIcon: "/icons/bottombar/deactive/walk.svg",
   },
   {
     name: "마이",
     path: "/my",
-    activeIcon: "/icons/bottombar/active/Account circle_aqua.png",
-    inactiveIcon: "/icons/bottombar/deactive/my_gray.png",
+    activeIcon: "/icons/bottombar/active/my_aqua.svg",
+    inactiveIcon: "/icons/bottombar/deactive/my.svg",
   },
 ];
 
@@ -40,18 +39,16 @@ const Bottombar = () => {
 
   if (hideOnPages.includes(pathname)) return null;
   return (
-    <nav className="fixed bottom-1.5 left-0 w-full bg-custom-white  shadow-[0_-1px_4px_rgba(0,0,0,0.05)] h-12">
+    <nav className="fixed bottom-1.5 left-0 w-full bg-custom-white shadow-[0_-1px_4px_rgba(0,0,0,0.05)] h-12">
       <div className="flex justify-around py-2">
         {navItems.map((item) => {
           const isActive = pathname.startsWith(item.path);
           return (
             <Link href={item.path} key={item.path} className="flex flex-col items-center gap-1">
-              <Image
-                className="h-5 w-5 object-contain aspect-square"
+              <img
+                className="h-5 w-5"
                 src={isActive ? item.activeIcon : item.inactiveIcon}
                 alt={item.name}
-                width={20}
-                height={20}
               />
               <span className={`text-[11.5px] ${isActive ? "text-aqua" : "text-custom-gray"}`}>
                 {item.name}
@@ -63,4 +60,5 @@ const Bottombar = () => {
     </nav>
   );
 };
+
 export default Bottombar;
