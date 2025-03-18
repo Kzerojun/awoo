9ca69b503b6ab152a29f74fbf5fbae35abbb18eb
daeun <danivalen@naver.com>
2025-03-18 15:00:53 +0900

    ✨ [FE] feat: 공통 컴포넌트 - 상단바 제작, 테스트 완료

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -1,11 +1,19 @@
-import Button from "../../common/Button";
+import TopBar from "@/common/ui/TopBar";
+import { BellIcon } from "@heroicons/react/24/outline"; // Heroicons 아이콘 추가
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <h1 className="text-4xl font-bold text-center">AwOO 프로젝트</h1>
-      <p className="text-lg mt-2 text-red-500">프로젝트 설명을 여기에 추가하세요 아좌좌 홧팅.</p>
-      <Button text="왜 안돼" backgroundColor="light-aqua" fontColor="green" />
-    </main>
+    <div className="relative min-h-screen bg-white">
+      {/* 상단바 */}
+      <TopBar title="페이지 제목" rightAction={<BellIcon className="h-6 w-6 text-gray-500" />} />
+
+      {/* 메인 컨텐츠 - 중앙 정렬 */}
+      <main className="flex flex-col items-center justify-center h-full min-h-[calc(100vh-3rem)]">
+        <div className="flex items-center space-x-2">
+          <h1 className="text-4xl font-bold text-center">AwOO</h1>
+        </div>
+        <p className="text-gray-500 mt-2">새로운 발자국 금융 라이프</p>
+      </main>
+    </div>
   );
 }
