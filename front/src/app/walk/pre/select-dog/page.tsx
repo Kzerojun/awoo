bd913f09dfc122c0f5ad3b1d1ad0d58ffa3c9976
강은수 <kes2450@naver.com>
2025-03-20 14:20:40 +0900

    ➕ [FE] add: 산책 전 강아지 선택 페이지 추가

--- front/src/app/walk/pre/select-dog/page.tsx
+++ front/src/app/walk/pre/select-dog/page.tsx
@@ -8,16 +8,16 @@ import Button from "@/common/ui/Button";
 const WalkPreSelectDogPage = () => {
   const route = useRouter();
 
-  const goToPhoto = () => {
-    route.push("/walk/start/photo");
+  const goToWalkStart = () => {
+    route.push("/walk/start/walking");
   };
 
   return (
     <>
       <TopBar title="산책" />
-      <div className="flex flex-col items-center mt-15 px-4">
+      <div className="flex flex-col items-center mt-4 px-4">
         <div>산책 전 강아지 선택 페이지</div>
-        <Button text="산책하러 가기" backgroundColor="green" onClick={goToPhoto} />
+        <Button text="산책하러 가기" backgroundColor="green" onClick={goToWalkStart} />
       </div>
     </>
   );
