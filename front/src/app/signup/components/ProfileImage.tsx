7637b581a4059131c50f8303c86bc90d69333e73
강은수 <kes2450@naver.com>
2025-03-21 14:16:53 +0900

    ➕ [FE] add: 프로필 이미지, 프로필 닉네임 등록 컴포넌트 파일 추가

---
+++ front/src/app/signup/components/ProfileImage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ProfileImage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div>프로필 이미지 등록</div>
+    </div>
+  );
+};
+
+export default ProfileImage;
---
+++ front/src/app/signup/components/ProfileNickname.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ProfileNickname = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div>프로필 닉네임 등록</div>
+    </div>
+  );
+};
+
+export default ProfileNickname;
