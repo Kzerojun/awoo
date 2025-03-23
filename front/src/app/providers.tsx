848fe51fdb798c014e35246dcb7114e791ad8420
강은수 <kes2450@naver.com>
2025-03-24 01:48:15 +0900

    ✨ [FE] feat: 리액트 쿼리 사용할 때 설정 추가

--- front/src/app/providers.tsx
+++ front/src/app/providers.tsx
@@ -1,8 +1,19 @@
 "use client"; // 중요: 클라이언트 컴포넌트로 지정
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
 import { Provider } from "react-redux";
 import { store } from "@/lib/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  const [queryClient] = useState<QueryClient>(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>{children}</Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
