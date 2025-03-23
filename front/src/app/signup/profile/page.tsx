8b8beabf2edcef38f9f85786e857abd60ec713c4
강은수 <kes2450@naver.com>
2025-03-23 19:24:14 +0900

    🚧 [FE] cont: 프로필 등록 페이지 props로 프로필 이미지, 닉네임 내려주기

--- front/src/app/signup/profile/page.tsx
+++ front/src/app/signup/profile/page.tsx
@@ -5,14 +5,19 @@ import React, { useState } from "react";
 import { useSwipeable } from "react-swipeable";
 import ProfileImage from "../components/ProfileImage";
 import ProfileNickname from "../components/ProfileNickname";
-import TopBar from "@/common/ui/TopBar";
-
-const pages = [
-  { id: 0, content: ProfileImage },
-  { id: 1, content: ProfileNickname },
-];
 
 const ProfileRegistPage = () => {
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const [nickname, setNickname] = useState<string>("");
+
+  const pages = [
+    {
+      id: 0,
+      content: () => <ProfileImage imagePreview={imagePreview} setImagePreview={setImagePreview} />,
+    },
+    { id: 1, content: () => <ProfileNickname nickname={nickname} setNickname={setNickname} /> },
+  ];
+
   const [page, setPage] = useState<number>(0);
   const [direction, setDirection] = useState<number>(0);
 
@@ -46,38 +51,42 @@ const ProfileRegistPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <TopBar title="프로필 등록" />
-      <div {...handlers} className="relative w-full h-[calc(100vh-56px)] overflow-hidden">
-        <AnimatePresence initial={false} custom={direction}>
-          <motion.div
-            key={page}
-            className="absolute w-full h-full"
-            custom={direction}
-            variants={variants}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            transition={{ duration: 0.3 }}
-          >
-            {React.createElement(pages[page].content)}
-          </motion.div>
-        </AnimatePresence>
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <div
+          {...handlers}
+          className=" pb-14 relative w-full h-[calc(100vh-56px)] overflow-y-auto overflow-x-hidden"
+        >
+          <AnimatePresence initial={false} custom={direction}>
+            <motion.div
+              key={page}
+              className="absolute w-full h-full"
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{ duration: 0.3 }}
+            >
+              {pages[page].content()}
+            </motion.div>
+          </AnimatePresence>
 
-        {/* 페이지 인디케이터 */}
-        <div className="absolute bottom-20 left-0 right-0 flex justify-center gap-2">
-          {pages.map((_, index) => (
-            <button
-              key={index}
-              onClick={() => changePage(index, index > page ? 1 : -1)}
-              className={`w-3 h-3프 rounded-full transition-all duration-300 ${
-                index === page ? "bg-aqua scale-110" : "bg-custom-gray"
-              }`}
-            />
-          ))}
+          {/* 페이지 인디케이터 */}
+          <div className="absolute bottom-5 left-0 right-0 flex justify-center gap-2 z-20">
+            {pages.map((_, index) => (
+              <button
+                key={index}
+                onClick={() => changePage(index, index > page ? 1 : -1)}
+                className={`w-3 h-3 rounded-full transition-all duration-300 ${
+                  index === page ? "bg-aqua scale-110" : "bg-custom-gray"
+                }`}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
