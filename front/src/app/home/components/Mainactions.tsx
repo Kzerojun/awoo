7c9ca2212de5b07f3fb2be7b2afe958592cca124
daeun <danivalen@naver.com>
2025-03-23 13:24:01 +0900

    ✅ [FE] update: 입출금 계좌 개설 (상품 설명 페이지) 이동 코드 추가

--- front/src/app/home/components/Mainactions.tsx
+++ front/src/app/home/components/Mainactions.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function MainActions() {
+  const router = useRouter();
   return (
     <div className="flex justify-between space-x-2 my-2">
       {/* 입출금 계좌 개설 버튼 */}
       <button
         type="button"
+        onClick={() => router.push("account/open/deposit")} // 입출금 상품 설명 페이지로 이동
         className="flex-1 p-3 bg-gray-100 rounded-lg shadow flex items-center justify-center gap-2 h-14"
       >
         <img src="/icons/main/money.svg" alt="입출금 아이콘" className="w-6 h-6" />
