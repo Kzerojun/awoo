1ba718c12e21e1927260a2fd5e82539187f11004
daeun <danivalen@naver.com>
2025-03-24 10:26:23 +0900

    ✨ [FE] feat: 적금(1단계) 상품 요약 컴포넌트 구현 완료

---
+++ front/src/app/account/open/saving/stage1/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import ProductSummary from "../components/ProductSummary";
+import SavingWalkCondition from "../components/SavingWalkCondition";
+const Page = () => {
+  return (
+    <div className="container mx-auto max-w-lg py-6">
+      <ProductSummary productName="산뜻하개" description="산책 적금 1단계 상품" interest="2.3%" />
+    </div>
+  );
+};
+export default Page;
