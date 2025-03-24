c2a524ee2a2708b3c20ee7f8b75c8dbbbc6dc978
deokjin <ghaehfdl7@naver.com>
2025-03-24 16:14:59 +0900

    ➖ [BE] remove: 카프카 관련 파일 주석 처리

--- back/member/src/main/java/com/awoo/member/ui/controller/KafkaController.java
+++ back/member/src/main/java/com/awoo/member/ui/controller/KafkaController.java
@@ -1,26 +1,26 @@
-package com.awoo.member.ui.controller;
-
-
-import com.awoo.member.domain.model.vo.UserKey;
-import com.awoo.member.infra.Kafka.KafkaProducer;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/kafka")
-@RequiredArgsConstructor
-public class KafkaController {
-
-    private final KafkaProducer kafkaProducer;
-
-    // kafka 메시지를 보내는 함수
-    @PostMapping("/send")
-    public ResponseEntity<?> send(@RequestBody UserKey dto) {
-        kafkaProducer.send("test-topic", dto);
-        return ResponseEntity.ok("ok");
-    }
-}
+//package com.awoo.member.ui.controller;
+//
+//
+//import com.awoo.member.domain.model.vo.UserKey;
+//import com.awoo.member.infra.Kafka.KafkaProducer;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/kafka")
+//@RequiredArgsConstructor
+//public class KafkaController {
+//
+//    private final KafkaProducer kafkaProducer;
+//
+//    // kafka 메시지를 보내는 함수
+//    @PostMapping("/send")
+//    public ResponseEntity<?> send(@RequestBody UserKey dto) {
+//        kafkaProducer.send("test-topic", dto);
+//        return ResponseEntity.ok("ok");
+//    }
+//}
