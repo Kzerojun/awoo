8433479097d90bb1b09651f7119d8e905e11c4e5
daeun <danivalen@naver.com>
2025-03-24 10:28:57 +0900

    ✨ [FE] feat: 적금 상품 요약 (2단계, 3단계) 구현 완료

--- front/src/app/account/open/saving/stage3/page.tsx
+++ front/src/app/account/open/saving/stage3/page.tsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import ProductSummary from "../components/ProductSummary";
+
 const Page = () => {
   return (
     <div className="container mx-auto max-w-lg py-6">
-      <ProductSummary productName="산뜻하개" description="산책 적금 1단계 상품" interest="2.3%" />
+      <ProductSummary productName="풍족하개" description="산책 적금 3단계 상품" interest="3.3%" />
     </div>
   );
 };
+
 export default Page;
