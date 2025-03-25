e55b572a79dfd29db9105d43916e45d14faf1766
김지한 <wlgks7460@naver.com>
2025-03-25 13:53:00 +0900

    ➕ [BE] add: Calendar 도메인 파일 추가

--- back/pet/src/main/java/com/awoo/pet/ui/facade/dto/response/RegisterPetResponse.java
+++ back/pet/src/main/java/com/awoo/pet/ui/facade/dto/response/RegisterPetResponse.java
@@ -1,7 +1,6 @@
 package com.awoo.pet.ui.facade.dto.response;
 
 import com.awoo.pet.domain.pet.Pet;
-import com.awoo.pet.ui.facade.dto.response.common.PetResponse;
 import lombok.Builder;
 import lombok.Data;
 
