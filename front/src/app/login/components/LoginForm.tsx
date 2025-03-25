9ae6e236ec9e924a657c80e363926cbdb2de209a
강은수 <kes2450@naver.com>
2025-03-26 01:24:51 +0900

    ✨ [FE] feat: 로그인 에러 처리

--- front/src/app/login/components/LoginForm.tsx
+++ front/src/app/login/components/LoginForm.tsx
@@ -1,15 +1,24 @@
 "use Client";
 
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useEffect } from "react";
 import Button from "../../../common/ui/Button";
 import paw from "../../../../public/icons/white_paw.svg";
 import Link from "next/link";
 import { useLogin } from "@/hooks/user/useLogin";
+import { useUserInfo } from "@/hooks/user/useUserInfo";
 
 const LoginForm = () => {
-  const { mutate: loginMutate, isPending, isError } = useLogin();
+  const { refetch: refetchUserInfo } = useUserInfo();
+  const { mutate: loginMutate, isPending, isError } = useLogin(refetchUserInfo);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+
+  // 로그인 에러 시
+  useEffect(() => {
+    if (isError) {
+      alert("로그인에 실패했습니다. 이메일 또는 비밀번호를 확인하세요.");
+    }
+  }, [isError]);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
