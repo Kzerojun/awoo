8f2f3dd1fe1acab64582c16546cea8106b0352c0
강은수 <kes2450@naver.com>
2025-03-26 01:24:28 +0900

    ✨ [FE] feat: 로그인 후 유저 정보 조회 기능 구현

--- front/src/hooks/user/useLogin.ts
+++ front/src/hooks/user/useLogin.ts
@@ -3,18 +3,30 @@ import { login } from "@/api/user/auth";
 import { useAppDispatch } from "@/lib/store";
 import { setUserData } from "@/lib/slices/userSlice";
 
-export const useLogin = () => {
+export const useLogin = (refetchUserInfo: () => Promise<any>) => {
   const dispatch = useAppDispatch();
   return useMutation({
     mutationFn: login,
-    onSuccess: (res) => {
+    onSuccess: async (res) => {
       console.log("로그인 성공", res);
       // 추가 (토큰 저장)
       const accessToken = res?.headers?.authorization;
-      localStorage.setItem("accessToken", accessToken);
+      if (accessToken) {
+        localStorage.setItem("accessToken", accessToken);
+      }
+
+      // 유저 정보 조회 refetch
+      const { data: userData } = await refetchUserInfo();
+
       // 스토어에도 저장
       dispatch(
         setUserData({
+          nickname: userData.nickname,
+          name: userData.name,
+          email: userData.email,
+          phone: userData.phone,
+          birthDate: userData.birthDate,
+          profileImage: userData.profileImage,
           accessToken: accessToken,
         })
       );
