d77cad1eb31a06a97f9dd0c07ef4afa0893f7629
김지한 <wlgks7460@naver.com>
2025-03-25 13:42:36 +0900

    로컬 변경 사항 커밋

--- back/account/src/main/java/com/awoo/account/infra/JpaAccountRepository.java
+++ back/account/src/main/java/com/awoo/account/infra/JpaAccountRepository.java
@@ -3,6 +3,8 @@ package com.awoo.account.infra;
 import com.awoo.account.domain.AccountEntity;
 import com.awoo.account.domain.AccountRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaAccountRepository extends JpaRepository<AccountEntity,Integer>, AccountRepository {
 }
