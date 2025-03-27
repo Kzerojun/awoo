7f1240dd1942a578e881819a7e8ff2c01d69e605
강은수 <kes2450@naver.com>
2025-03-27 22:12:56 +0900

    ➕ [FE] add: 적금 조회 페이지

--- front/src/app/account/my/saving/page.tsx
+++ front/src/app/account/my/saving/page.tsx
@@ -1,11 +1,20 @@
 "use client";
+import React from "react";
+import SavingTop from "../components/SavingTop";
+import SavingBottom from "../components/SavingBottom";
+import { MockSavingInfo } from "../components/MockSaving";
+import { SavingAccountInfo } from "../components/MyAccountType";
+
+interface MySavingPageProps {
+  savingInfo: SavingAccountInfo;
+}
 
 const MySavingPage = () => {
   return (
-    <>
-      <div>내 적금 조회 페이지입니다.</div>
-      <div>적금 조회</div>
-    </>
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      {/* <SavingTop savingInfo={savingInfo} />
+      <SavingBottom savingInfo={savingInfo} /> */}
+    </div>
   );
 };
 
