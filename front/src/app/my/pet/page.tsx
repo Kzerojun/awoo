4931a7869db31d922a5c631ded3b3d2c11b026ca
강은수 <kes2450@naver.com>
2025-03-28 17:32:38 +0900

    🚧 [FE] cont: 펫 관련 수정

--- front/src/app/my/pet/page.tsx
+++ front/src/app/my/pet/page.tsx
@@ -3,19 +3,40 @@
 import CommonTopBar from "@/common/ui/CommonTopBar";
 import { useAppDispatch, useAppSelector } from "@/lib/store";
 import { changeMyPetView } from "@/lib/slices/userActionSlice";
-import { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
+import { usePetList } from "@/hooks/pet/usePetList";
+import { setPetList } from "@/lib/slices/petSlice";
 import NoPet from "./components/NoPet";
 import PetMain from "./components/PetMain";
+import { PetInterface } from "@/lib/slices/petSlice";
 
 const Pet = () => {
   const dispatch = useAppDispatch();
   // const currentView = 2; // 테스트를 위한 코드 (펫 메인페이지)
+  const { refetch: petListRefetch, isLoading: petListIsLoading } = usePetList();
   const currentView = useAppSelector((state) => state.userAction.currentMyPetView);
-  const petList = useAppSelector((state) => state.user.petList);
-  const petNum = petList?.length;
+  // const petList = useAppSelector((state) => state.pet.petList);
+  const [localPetList, setLocalPetList] = useState<PetInterface[] | null>(null);
+  const petNum = localPetList?.length;
 
   // 반려견 정보 조회 useEffect 로
+  useEffect(() => {
+    const fetchPetList = async () => {
+      try {
+        const result = await petListRefetch();
+        console.log("반려견 목록 조회", result.data);
+        console.log("반려견 목록에서 펫 데이터", result?.data);
+
+        if (result.isSuccess && result.data) {
+          setLocalPetList(result.data);
+          dispatch(setPetList(result.data));
+        }
+      } catch (err) {
+        console.error("반려견 목록 조회 실패임요!", err);
+      }
+    };
+    fetchPetList();
+  }, []);
 
   useEffect(() => {
     if (petNum === 0) {
