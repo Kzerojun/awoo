5498e8ae808c40bf8eb7796842e373b80f1ef107
daeun <danivalen@naver.com>
2025-03-29 12:48:11 +0900

    ✅ [FE] update: 파일 import 경로 수정

--- front/src/app/account/verify/success/deposit/page.tsx
+++ front/src/app/account/verify/success/deposit/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import CommonTopBar from "@/common/ui/CommonTopBar";
-import Checkmark from "../../../../../public/icons/mypage/checkmark.svg";
+import Checkmark from "../../../../../../public/icons/mypage/checkmark.svg";
 import AccountComplete from "../../../open/components/AccountComplete";
 import Button from "@/common/ui/Button";
 
