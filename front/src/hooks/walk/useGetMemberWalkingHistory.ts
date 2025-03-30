12e3e252f1e0f8a704bd19e27f081f7d7de90310
강은수 <kes2450@naver.com>
2025-03-30 16:38:28 +0900

    ✨ [FE] feat: 멤버별 산책 기록 조회 쿼리 작성

---
+++ front/src/hooks/walk/useGetMemberWalkingHistory.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { getMemberWalkingHistory } from "@/api/walk/walk";
+
+export const useGetMemberWalkingHistory = (memberId: number) => {
+    return useQuery({
+        queryKey: ["getMemberWalkingHistory", memberId],
+        queryFn: () => getMemberWalkingHistory(memberId);
+        enabled: !!memberId,
+        refetchOnWindowFocus: false,
+    })
+}
