06c8d33b78d1e8970aea465f93fdb5d9bb339453
강은수 <kes2450@naver.com>
2025-03-30 19:04:45 +0900

    ✅ [FE] update: queryFn 수정

--- front/src/hooks/walk/useGetMemberWalkingHistory.ts
+++ front/src/hooks/walk/useGetMemberWalkingHistory.ts
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { getMemberWalkingHistory } from "@/api/walk/walk";
 
 export const useGetMemberWalkingHistory = (memberId: number) => {
-    return useQuery({
-        queryKey: ["getMemberWalkingHistory", memberId],
-        queryFn: () => getMemberWalkingHistory(memberId);
-        enabled: !!memberId,
-        refetchOnWindowFocus: false,
-    })
-}
+  return useQuery({
+    queryKey: ["getMemberWalkingHistory", memberId],
+    queryFn: () => getMemberWalkingHistory(memberId),
+    enabled: !!memberId,
+    refetchOnWindowFocus: false,
+  });
+};
