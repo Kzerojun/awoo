9d95126ce4296b0b2342779cc7e87ff7b718d8a8
강은수 <kes2450@naver.com>
2025-03-30 19:02:38 +0900

    🚧 [FE] cont: refreshToken 요청 일단 주석처리

--- front/src/api/axiosInstance.ts
+++ front/src/api/axiosInstance.ts
@@ -21,35 +21,35 @@ axiosInstance.interceptors.request.use(
 );
 
 // 401 에러 (accessToken값 만료시) -> refresh 시도
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
 
-    if (
-      (error.response?.status === 401 || error.response?.status === 419) &&
-      !originalRequest._retry // 무한 루프 방지
-    ) {
-      originalRequest._retry = true;
+//     if (
+//       (error.response?.status === 401 || error.response?.status === 419) &&
+//       !originalRequest._retry // 무한 루프 방지
+//     ) {
+//       originalRequest._retry = true;
 
-      try {
-        const res = await refreshToken();
-        const newAccessToken = res.data.accessToken;
+//       try {
+//         const res = await refreshToken();
+//         const newAccessToken = res.data.accessToken;
 
-        // 새 accessToken 저장
-        localStorage.setItem("accessToken", newAccessToken);
+//         // 새 accessToken 저장
+//         localStorage.setItem("accessToken", newAccessToken);
 
-        originalRequest.headers.authorization = `Bearer ${newAccessToken}`;
-        return axiosInstance(originalRequest);
-      } catch (err) {
-        localStorage.removeItem("accessToken");
-        toast.error("세션이 만료되었습니다. 다시 로그인 해주시길 바랍니다.");
-        window.location.href = "/login";
-        return Promise.reject(err);
-      }
-    }
-    return Promise.reject(error);
-  }
-);
+//         originalRequest.headers.authorization = `Bearer ${newAccessToken}`;
+//         return axiosInstance(originalRequest);
+//       } catch (err) {
+//         localStorage.removeItem("accessToken");
+//         toast.error("세션이 만료되었습니다. 다시 로그인 해주시길 바랍니다.");
+//         window.location.href = "/login";
+//         return Promise.reject(err);
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosInstance;
