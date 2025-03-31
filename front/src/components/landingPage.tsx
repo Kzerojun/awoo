eacf3b8f0c38cfc301f8029ce71a475454a077bc
kimhongbeom <fkdldj48@naver.com>
2025-03-31 11:19:53 +0900

    ✨ [FE] feat: 랜딩 페이지 UI

---
+++ front/src/components/landingPage.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import landingLogo from "../../public/logos/AwOO_landing.svg";
+import Image from "next/image";
+
+export default function LandingPage() {
+  return (
+    <div className="flex flex-col min-h-screen bg-[#0FC9BA] justify-center items-center">
+      <div className="flex flex-col items-center w-full max-w-[250px]">
+        <div className="self-start text-white text-xl">새로운</div>
+        <div className="self-start text-white text-xl mb-3">
+          <span className="font-bold text-2xl">발자국</span> 금융 라이프
+        </div>
+        <Image src={landingLogo} alt=".." width={250} height={250} className="mb-10" />
+      </div>
+    </div>
+  );
+}
