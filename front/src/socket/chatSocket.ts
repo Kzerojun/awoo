bf0c2466ca671c1f3a4d6665b2dafd3140aac49b
daeun <danivalen@naver.com>
2025-04-01 17:51:26 +0900

    ✨ [FE] feat: 채팅 관련 redux 파일 생성

---
+++ front/src/socket/chatSocket.ts
@@ -0,0 +1,69 @@
+// src/socket/chatSocket.ts
+import SockJS from "sockjs-client";
+import { Client, IMessage } from "@stomp/stompjs";
+
+class ChatSocket {
+  private stompClient: Client | null = null;
+
+  // src/socket/chatSocket.ts
+  connect(token: string, roomId: number, callback: (message: IMessage) => void) {
+    if (this.stompClient && this.stompClient.connected) return;
+
+    const socket = new SockJS(`${process.env.NEXT_PUBLIC_API_BASE_URL}/used-products/ws`);
+    this.stompClient = new Client({
+      webSocketFactory: () => socket,
+      connectHeaders: {
+        Authorization: `${token}`,
+      },
+      debug: (str) => console.log("[STOMP]", str),
+      reconnectDelay: 5000,
+      onConnect: () => {
+        console.log("✅ STOMP 연결 성공");
+        this.subscribe(roomId, callback);
+      },
+    });
+
+    this.stompClient.activate();
+  }
+
+  subscribe(roomId: number, callback: (message: IMessage) => void) {
+    if (!this.stompClient || !this.stompClient.connected) return;
+
+    this.stompClient.subscribe(`/sub/chat/rooms/${roomId}`, (message) => {
+      console.log("💬 수신된 메시지", message);
+      callback(message);
+    });
+
+    console.log("✅ 채팅방 구독 완료 - roomId:", roomId);
+  }
+
+  send(roomId: number, message: string, image: string | null = null) {
+    if (!this.stompClient || !this.stompClient.connected) return;
+    const token = localStorage.getItem("accessToken");
+    console.log(`메시지 전송: ${message}`);
+
+    // const userId = useSelector((state: RootState) => state.user.userId); // userId를 Redux에서 가져옵니다.
+
+    // if (!userId) {
+    //   console.error("User ID is missing. Cannot send message.");
+    //   return;
+    // }
+    // ✅ image 기본 null 적용
+    this.stompClient.publish({
+      destination: `/pub/chat/rooms/${roomId}`,
+      headers: {
+        Authorization: `${token}`, // 인증용
+      },
+      body: JSON.stringify({ message: "dfdfd", image: "" }),
+    });
+    console.log("🚀 메시지 발송");
+  }
+
+  disconnect() {
+    this.stompClient?.deactivate();
+    this.stompClient = null;
+    console.log("❌ 연결 종료");
+  }
+}
+
+export const chatSocket = new ChatSocket();
