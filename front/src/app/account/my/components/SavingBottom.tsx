52be7d647b09f9031b0d2d8c6b47353fa469fa62
강은수 <kes2450@naver.com>
2025-04-01 16:17:55 +0900

    ♻️ [FE] refactor: 적금 조회 하단 부분 기능 연결

--- front/src/app/account/my/components/SavingBottom.tsx
+++ front/src/app/account/my/components/SavingBottom.tsx
@@ -2,14 +2,17 @@
 
 import React from "react";
 import { SavingAccountInfo } from "./MyAccountType";
+import { SavingResponse } from "@/api/account/my/saving";
+import SavingList from "./SavingList";
 
 interface MySavingProps {
   savingInfo: SavingAccountInfo;
 }
+
 const SavingBottom = ({ savingInfo }: MySavingProps) => {
   return (
-    <div>
-      <div>적금 아래 부분</div>
+    <div className="flex-1 w-full flex flex-col ">
+      <SavingList accountNo={savingInfo.accountNo} />
     </div>
   );
 };
