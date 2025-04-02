233ab5d2ebad65ceba512ea284cc3674e6714281
daeun <danivalen@naver.com>
2025-04-02 21:40:20 +0900

    ♻️ [FE] refactor: 테스트 코드 삭제

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -4,10 +4,8 @@ import Header from "@/app/home/components/Header";
 import SavingRecommendation from "./components/SavingRecommendation";
 import MainActions from "./components/Mainactions";
 import InfoCard from "./components/InfoCard";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
   return (
     <div className="flex flex-col">
       {/* 네비게이션 바 */}
@@ -34,7 +32,6 @@ export default function Home() {
             iconSrc="/icons/main/scales.svg"
           />
         </div>
-        <div onClick={() => router.push(`/alarm`)}>알림 테스트</div>
       </div>
     </div>
   );
