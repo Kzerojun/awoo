038a21ed60dd72c47c9a58510387f46b31f20452
kimhongbeom <fkdldj48@naver.com>
2025-04-02 23:56:20 +0900

    ➖ [FE] remove: 내 문의 내역 보기 삭제

--- front/src/app/my/question/page.tsx
+++ front/src/app/my/question/page.tsx
@@ -17,13 +17,8 @@ export default function Question() {
           <p className="text-gray-600 mb-4 text-lg">도움이 필요하신가요?</p>
           <div className="flex gap-2">
             <Link href="/my/question/questioning" className="flex-1">
-              <button className="w-full py-2 bg-[#B2B2B2] rounded-md text-white text-sm">
+              <button className="w-full py-3 bg-[#B2B2B2] rounded-md text-white text-sm">
                 문의하기
-              </button>
-            </Link>
-            <Link href="/my/question/myQuestion" className="flex-1">
-              <button className="w-full py-2 bg-[#B2B2B2] rounded-md text-white text-sm">
-                문의 내역 보기
               </button>
             </Link>
           </div>
