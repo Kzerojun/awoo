c6a00793977ba3d028336de39ecedde9b650d22f
강은수 <kes2450@naver.com>
2025-04-02 10:52:52 +0900

    ✨ [FE] feat: 계좌이체 단계에 따른 컴포넌트 렌더링 처리

--- front/src/app/account/my/deposit/transfer/page.tsx
+++ front/src/app/account/my/deposit/transfer/page.tsx
@@ -5,11 +5,17 @@ import React, { useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "@/lib/store";
 import { checkPasswordConfirm } from "@/lib/slices/userActionSlice";
 import { useRouter } from "next/navigation";
+import TransferStep1 from "../../components/TransferStep1";
+import TransferStep2 from "../../components/TransferStep2";
+import TransferStep3 from "../../components/TransferStep3";
+import TransferStep4 from "../../components/TransferStep4";
+import WalkingLoading from "@/app/walk/components/WalkingLoading";
 
 const TransferPage = () => {
   const dispatch = useAppDispatch();
   const router = useRouter();
   const confirmPassword = useAppSelector((state) => state.userAction.checkPassword);
+  const transferStep = useAppSelector((state) => state.transfer.transferStep);
 
   // 비밀번호 확인 없이는 못 들어오게 처리
   // useEffect(() => {
@@ -28,10 +34,20 @@ const TransferPage = () => {
   // if (confirmPassword === false) {
   //   return null;
   // }
+
+  const TransferStepComponent = {
+    1: <TransferStep1 />,
+    2: <TransferStep2 />,
+    3: <TransferStep3 />,
+    4: <TransferStep4 />,
+  }[transferStep] || <WalkingLoading />;
+
   return (
     <div>
-      <CommonTopBar title="계좌 이체" rightAction="cancel" />
-      <main className="mt-14 flex flex-col justify-center items-center"></main>
+      {transferStep !== 4 && <CommonTopBar title="계좌 이체" rightAction="cancel" />}
+      <main className="mt-14 flex flex-col justify-center items-center">
+        {TransferStepComponent}
+      </main>
     </div>
   );
 };
