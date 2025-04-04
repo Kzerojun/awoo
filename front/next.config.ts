13bc3d514d9ea13a4550cfb17cad66b2aa29bd0c
kimhongbeom <fkdldj48@naver.com>
2025-04-04 12:59:14 +0900

    ✅ [FE] update: config 파일 수정

--- front/next.config.ts
+++ front/next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
   },
   // 이미지 도메인 설정 추가
   images: {
-    domains: ["c209awoo.s3.us-east-2.amazonaws.com", "test.com"],
+    domains: ["c209awoo.s3.us-east-2.amazonaws.com"],
   },
   // ✅ next-pwa 설정은 별도로 적용
   ...withPWA({
