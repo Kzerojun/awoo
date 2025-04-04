564d09a90ccdcdc6e964c4882be06497491f7261
deokjin <ghaehfdl7@naver.com>
2025-04-04 15:52:36 +0900

    ✨ [BE] feat: 약관 미동의 시 error 반환

--- back/account/src/main/java/com/awoo/account/application/exception/AccountApplicationErrorCode.java
+++ back/account/src/main/java/com/awoo/account/application/exception/AccountApplicationErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 public enum AccountApplicationErrorCode {
 
     ACCOUNT_NOT_FOUND(HttpStatus.BAD_REQUEST, "계좌를 찾을 수 없습니다."),
-    INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST,"계좌 잔액이 충분하지 않습니다.");
+    INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST,"계좌 잔액이 충분하지 않습니다."),
+    CONDITION_FALSE(HttpStatus.BAD_REQUEST, "약관 미동의");
+
 
     private final HttpStatus httpStatus;
     private final String message;
