34620caf8ab9024ecb900d6495f3ca07a54ce328
daeun <danivalen@naver.com>
2025-04-05 17:59:26 +0900

    ✅ [FE] update: 중고거래 진입시 멍페이 가입 여부에 따른 분기

--- front/src/app/market/page.tsx
+++ front/src/app/market/page.tsx
@@ -9,13 +9,24 @@ import ChatList from "./components/ChatList";
 import Button from "@/common/ui/Button";
 import { useRouter } from "next/navigation";
 import { getProductList } from "@/api/market/read/getList";
+import MoungpayJoinModal from "@/app/market/components/MoungpayJoinModel";
+import { useAppSelector } from "@/lib/store";
 
 export default function MarketPage() {
   const [currentTab, setCurrentTab] = useState<MarketTab>("상품");
   const [items, setItems] = useState<MarketItem[]>([]);
   const [originalItems, setOriginalItems] = useState<MarketItem[]>([]);
   const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
+  const isPaymentUser = useAppSelector((state) => state.user.paymentRegister);
 
+  const handleWriteClick = () => {
+    if (!isPaymentUser) {
+      setShowModal(true); // 모달 띄우기
+      return;
+    }
+    router.push("/market/article/write"); // 멍페이 가입자만 진입
+  };
   // ✅ 목록 API 연동
   useEffect(() => {
     const fetchData = async () => {
@@ -80,8 +91,9 @@ export default function MarketPage() {
         className="fixed bottom-16 right-3 shadow-lg z-50"
         backgroundColor="aqua"
         fontColor="custom-white"
-        onClick={() => router.push("/market/article/write")}
+        onClick={handleWriteClick}
       />
+      <MoungpayJoinModal isOpen={showModal} onClose={() => setShowModal(false)} />
     </div>
   );
 }
