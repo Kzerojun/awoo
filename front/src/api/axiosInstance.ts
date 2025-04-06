19edfe3f2027d03d7d22bbe054db0caafde3455f
강은수 <kes2450@naver.com>
2025-04-07 01:41:12 +0900

    ✨ [FE] feat: 리프레시 토큰 관련 코드 주석 해제

--- front/src/api/axiosInstance.ts
+++ front/src/api/axiosInstance.ts
@@ -7,20 +7,20 @@ const axiosInstance = axios.create({
   withCredentials: true, // refreshToken 쿠기 보내기
 });
 
-// let isRefreshing = false;
-// let failedQueue: any[] = [];
+let isRefreshing = false;
+let failedQueue: any[] = [];
 
-// const processQueue = (error: any, token: string | null = null) => {
-//   failedQueue.forEach((prom) => {
-//     if (error) {
-//       prom.reject(error);
-//     } else {
-//       prom.resolve(token);
-//     }
-//   });
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
 
-//   failedQueue = [];
-// };
+  failedQueue = [];
+};
 
 // axios 요청 시 헤더에 accessToken 자동으로 추가
 axiosInstance.interceptors.request.use(
@@ -36,55 +36,56 @@ axiosInstance.interceptors.request.use(
 );
 
 // 401 에러 (accessToken값 만료시) -> refresh 시도
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     const originalRequest = error.config;
-//     console.log("리프레쉬 토큰 확인");
-//     if (
-//       (error.response?.status === 401 || error.response?.status === 419) &&
-//       !originalRequest._retry // 무한 루프 방지
-//     ) {
-//       if (originalRequest.url.includes("/members/refresh")) {
-//         // refreshToken 자체가 실패한 경우 (세션 만료)
-//         localStorage.removeItem("accessToken");
-//         toast.error("세션이 만료되었습니다. 다시 로그인 해주세요.");
-//         window.location.href = "/login";
-//         return Promise.reject(error);
-//       }
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    console.log("리프레쉬 토큰 확인");
+    if (
+      (error.response?.status === 401 || error.response?.status === 419) &&
+      !originalRequest._retry // 무한 루프 방지
+    ) {
+      if (originalRequest.url.includes("/members/refresh")) {
+        // refreshToken 자체가 실패한 경우 (세션 만료)
+        localStorage.removeItem("accessToken");
+        alert("세션이 만료되었습니다. 다시 로그인 해주세요.");
+        window.location.href = "/login";
+        return Promise.reject(error);
+      }
 
-//       if (isRefreshing) {
-//         return new Promise((resolve, reject) => {
-//           failedQueue.push({ resolve, reject });
-//         })
-//           .then((token) => {
-//             originalRequest.headers.Authorization = `Bearer ${token as string}`;
-//             return axiosInstance(originalRequest);
-//           })
-//           .catch((err) => Promise.reject(err));
-//       }
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers.Authorization = `Bearer ${token as string}`;
+            return axiosInstance(originalRequest);
+          })
+          .catch((err) => Promise.reject(err));
+      }
 
-//       originalRequest._retry = true;
-//       isRefreshing = true;
+      originalRequest._retry = true;
+      isRefreshing = true;
 
-//       try {
-//         const res = await refreshToken();
-//         const newAccessToken = res.data.accessToken;
+      try {
+        const res = await refreshToken();
+        const newAccessToken = res.data.accessToken;
 
-//         // 새 accessToken 저장
-//         localStorage.setItem("accessToken", newAccessToken);
+        // 새 accessToken 저장
+        localStorage.setItem("accessToken", newAccessToken);
 
-//         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-//         return axiosInstance(originalRequest);
-//       } catch (err) {
-//         localStorage.removeItem("accessToken");
-//         toast.error("세션이 만료되었습니다. 다시 로그인 해주시길 바랍니다.");
-//         window.location.href = "/login";
-//         return Promise.reject(err);
-//       }
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        localStorage.removeItem("accessToken");
+        console.error("리프레시 토큰 실패:", err);
+        alert("세션이 만료되었습니다. 다시 로그인 해주시길 바랍니다.");
+        window.location.href = "/login";
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
