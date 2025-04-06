0ed7099bab3a8f05f832f1c2b253cb38d0e2520f
daeun <danivalen@naver.com>
2025-04-06 23:17:26 +0900

    ✅ [FE] update: 중고거래 세부수정

--- front/src/app/market/components/MarketListItem.tsx
+++ front/src/app/market/components/MarketListItem.tsx
@@ -3,9 +3,11 @@
 import Image from "next/image";
 import React from "react";
 import { MarketItem } from "../types/market";
-import { EyeIcon, ChatBubbleLeftEllipsisIcon } from "@heroicons/react/24/outline";
+import { EyeIcon } from "@heroicons/react/24/outline";
 import { BookmarkIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
+import { formatDistanceToNow, differenceInMinutes } from "date-fns";
+import { ko } from "date-fns/locale";
 interface MarketListItemProps extends MarketItem {
   onClick?: () => void;
 }
@@ -13,7 +15,7 @@ export default function MarketListItem({
   productId,
   imageUrl,
   title,
-  time,
+  createdAt,
   price,
   viewCount,
   likeCount,
@@ -25,6 +27,9 @@ export default function MarketListItem({
     if (onClick) return onClick(); // ✅ 부모에서 넘긴 onClick 있으면 실행
     router.push(`/market/${productId}`);
   };
+
+  console.log("createdAt 값:", createdAt, new Date(createdAt));
+
   return (
     <div onClick={handleClick} className="flex gap-3 border-b  border-gray-200 pb-4">
       {/* 썸네일 */}
@@ -36,18 +41,27 @@ export default function MarketListItem({
       <div className="flex-1 flex flex-col justify-between">
         <div>
           <p className="text-sm font-medium text-gray-900 line-clamp-2">{title}</p>
-          <p className="text-xs text-gray-400 mt-1">{time}</p>
+          <p className="text-xs text-gray-500 mt-1">
+            {differenceInMinutes(new Date(), new Date(createdAt)) < 1
+              ? "방금 전"
+              : formatDistanceToNow(new Date(createdAt), {
+                  addSuffix: true,
+                  locale: ko,
+                }).replace("약 ", "")}
+          </p>
         </div>
-        <div className="text-base font-bold text-gray-900 mt-1">{price}</div>
-        <div className="text-xs text-gray-400 mt-1 flex items-center space-x-2">
-          <span className="flex items-center gap-1">
-            <EyeIcon className="w-4 h-4" />
-            {viewCount}
-          </span>
-          <span className="flex items-center gap-1">
-            <BookmarkIcon className="w-4 h-4" />
-            {likeCount}
-          </span>
+        <div className="mt-1 flex items-center">
+          <div className="text-base font-bold text-gray-900">{price}원</div>
+          <div className="ml-auto flex items-center gap-2 text-xs text-gray-400">
+            <span className="flex items-center gap-1">
+              <EyeIcon className="w-4 h-4" />
+              {viewCount}
+            </span>
+            <span className="flex items-center gap-1">
+              <BookmarkIcon className="w-4 h-4" />
+              {likeCount}
+            </span>
+          </div>
         </div>
       </div>
     </div>
