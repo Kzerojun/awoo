d87898adee147eeb4c7b4c9af19de1c3bb525c81
강은수 <kes2450@naver.com>
2025-04-08 02:43:22 +0900

    ✨ [FE] feat: 캘린더 페이지에 캘린더 기능 추가

--- front/src/app/my/pet/calendar/page.tsx
+++ front/src/app/my/pet/calendar/page.tsx
@@ -1,10 +1,14 @@
 "use client";
+import CommonTopBar from "@/common/ui/CommonTopBar";
+import Calendar from "../components/Calendar";
 
 const CalendarPage = () => {
   return (
     <div>
-      캘린더 페이지
-      <div></div>
+      <CommonTopBar title="캘린더" backUrl="/my" />
+      <main className="flex flex-col items-center w-full min-h-[calc(100dvh-7rem)]">
+        <Calendar />
+      </main>
     </div>
   );
 };
