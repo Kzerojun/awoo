cefd29236119a5d2cc75360d49a0cb4cbe8a24cd
강은수 <kes2450@naver.com>
2025-04-07 15:38:57 +0900

    ✨ [FE] feat: 자동로그인 기능 추가

--- front/src/hooks/user/AppInitializer.tsx
+++ front/src/hooks/user/AppInitializer.tsx
@@ -1,39 +1,39 @@
-// "use client";
+"use client";
 
-// import { useEffect } from "react";
-// import { useUserInfo } from "./useUserInfo";
-// import { setUserData } from "@/lib/slices/userSlice";
-// import { useAppDispatch } from "@/lib/store";
-// import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useUserInfo } from "./useUserInfo";
+import { setUserData } from "@/lib/slices/userSlice";
+import { useAppDispatch } from "@/lib/store";
+import { useRouter } from "next/navigation";
 
-// const AppInitializer = () => {
-//   const { refetch: refetchUserInfo, isSuccess, isError } = useUserInfo();
-//   const dispatch = useAppDispatch();
-//   const router = useRouter();
+const AppInitializer = () => {
+  const { refetch: refetchUserInfo, isSuccess, isError } = useUserInfo();
+  const dispatch = useAppDispatch();
+  const router = useRouter();
 
-//   useEffect(() => {
-//     const accessToken = localStorage.getItem("accessToken");
-//     if (accessToken) {
-//       refetchUserInfo()
-//         .then(({ data }) => {
-//           console.log("이니셜라이저 실행");
-//           if (data) {
-//             dispatch(setUserData(data));
-//             router.replace("/home");
-//           }
-//         })
-//         .catch((err) => {
-//           console.error("자동 로그인 시 유저 정보 가져오기 실패: ", err);
-//           alert("로그아웃 되었습니다. \n 다시 로그인해주세요.");
-//           localStorage.removeItem("accessToken");
-//           router.replace("/login");
-//         });
-//     } else {
-//       router.replace("/login");
-//     }
-//   }, []);
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      refetchUserInfo()
+        .then(({ data }) => {
+          console.log("이니셜라이저 실행");
+          if (data) {
+            dispatch(setUserData(data));
+            router.replace("/home");
+          }
+        })
+        .catch((err) => {
+          console.error("자동 로그인 시 유저 정보 가져오기 실패: ", err);
+          alert("로그아웃 되었습니다. \n 다시 로그인해주세요.");
+          localStorage.removeItem("accessToken");
+          router.replace("/login");
+        });
+    } else {
+      router.replace("/login");
+    }
+  }, []);
 
-//   return null;
-// };
+  return null;
+};
 
-// export default AppInitializer;
+export default AppInitializer;
