5ccc9c3a544a8053d7cba52e46432181ca311857
강은수 <kes2450@naver.com>
2025-04-07 13:51:42 +0900

    💄 [FE] style: 디자인 및 산책 일수 수정

--- front/src/app/my/pet/components/PetWalkStatus.tsx
+++ front/src/app/my/pet/components/PetWalkStatus.tsx
@@ -1,27 +1,41 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useGetPetWalkingHistory } from "@/hooks/walk/useGetPetWalkingHistory";
+import { useGetPetWalkInMonthHistory } from "@/hooks/walk/useGetPetWalkInMonthHistory";
 interface PetWalkStatusProps {
   petId: number;
   name: string;
 }
 const PetWalkStatus = ({ petId, name }: PetWalkStatusProps) => {
   const { data: petWalkList, isLoading, isError } = useGetPetWalkingHistory(petId);
+  const {
+    data: petWalkInMonthData,
+    isLoading: inMonthLoading,
+    isError: inMonthError,
+  } = useGetPetWalkInMonthHistory(petId);
 
-  if (isLoading) return <div>{name} 로딩 중...</div>;
-  if (isError) return <div>{name} 산책 기록 불러오기 실패</div>;
+  useEffect(() => {
+    if (petWalkInMonthData) {
+      console.log("이번달 산책", petWalkInMonthData);
+    }
+  }, []);
 
-  const walkCountThisMonth =
+  if (isLoading || inMonthLoading) return <div>{name} 로딩 중...</div>;
+  if (isError || inMonthError) return <div>{name} 산책 기록 불러오기 실패</div>;
+
+  const walkAllCountThisMonth =
     petWalkList?.filter((walk) => {
       const date = new Date(walk.startTime);
       const now = new Date();
       return date.getFullYear() === now.getFullYear() && date.getMonth() === now.getMonth();
     }).length ?? 0;
 
+  const walkInMonth = petWalkInMonthData?.length;
+
   return (
     <div>
-      {name}: {walkCountThisMonth}회 / 30일
+      {walkInMonth}회 | {walkAllCountThisMonth}회
     </div>
   );
 };
