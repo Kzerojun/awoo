7bae5da2d86a6686ff29b2b33b5f5fbd5a6dd3d0
김지한 <wlgks7460@naver.com>
2025-04-08 11:21:38 +0900

    ♻️ [BE] refactor: walk 엔티티 수정

--- back/pet/src/main/java/com/awoo/pet/domain/walk/WalkFactory.java
+++ back/pet/src/main/java/com/awoo/pet/domain/walk/WalkFactory.java
@@ -8,13 +8,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class WalkFactory {
 
-    public Walk registerWalk(final RegisterWalkCommand command) {
+    public Walk registerWalk(final RegisterWalkCommand command, final String isSaving) {
         return Walk.builder()
                 .memberId(command.walkCommand().memberId())
                 .petId(command.walkCommand().petId())
                 .startTime(command.walkCommand().startTime())
                 .endTime(command.walkCommand().endTime())
                 .distance(command.walkCommand().distance())
+                .isSaving(isSaving)
                 .build();
     }
 }
