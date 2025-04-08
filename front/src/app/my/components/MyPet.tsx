184a01b6ab501aad2950955a882c744cce3f8ec8
강은수 <kes2450@naver.com>
2025-04-08 12:15:24 +0900

    💄 [FE] style: 마이펫에서 등록하기 버튼 수정 및 추가

--- front/src/app/my/components/MyPet.tsx
+++ front/src/app/my/components/MyPet.tsx
@@ -39,6 +39,9 @@ export default function MyPet() {
   const NoPetsMessage = () => (
     <div className="flex flex-col items-center justify-center w-full">
       <p className="text-sm text-gray-500 mb-2">등록된 반려동물이 없어요</p>
+      <p className="w-full text-aqua " onClick={() => router.push("/my/pet/register")}>
+        등록하기 +
+      </p>
     </div>
   );
 
@@ -53,10 +56,7 @@ export default function MyPet() {
           <div className="flex justify-between items-center">
             <div className="text-xs text-[#828282] ml-1">나의 반려동물 보러 가기</div>
             {petList && petList?.length <= 2 && (
-              <div
-                className="text-xs text-[#828282] "
-                onClick={() => router.push("/my/pet/register")}
-              >
+              <div className="text-xs text-aqua " onClick={() => router.push("/my/pet/register")}>
                 등록하기 +
               </div>
             )}
